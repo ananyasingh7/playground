61431543780d0e5875f222e64433d4f7cd8fabb4
Ananya Singh <shtoogienet@gmail.com>
2024-08-20 22:42:01 -0400

    some LL init

---
+++ linked-list/single-linked-list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def insertAtBegin(self, data):
+        pass
+
+    # Method to add a node at any index
+    # Indexing starts from 0.
+    def insertAtIndex(self, data, index):
+        pass
+
+    # Method to add a node at the end of LL
+    def insertAtEnd(self, data):
+        pass
+
+    # Update node of a linked list at given position
+    def updateNode(self, val, index):
+        pass
+
+    # Method to remove first node of linked list
+    def remove_first_node(self):
+        pass
+
+     # Method to remove a node from linked list
+    def remove_node(self, data):
+        pass
+
+    # Print the size of linked list
+    def sizeOfLL(self):
+        pass
